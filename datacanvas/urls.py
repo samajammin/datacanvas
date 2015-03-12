7c60ec9017553a38906b6fdb7fad6b62e8fba052
Sam Richards <sbrichards@gmail.com>
2015-03-12 15:16:41 -0700

    Add time filter views on API

--- datacanvas/urls.py
+++ datacanvas/urls.py
@@ -9,7 +9,10 @@ urlpatterns = patterns('',
     url(r'^$', 'soundscore.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # todo split up api urls & views if things get unwieldy
     # url(r'^api/v1/', include('soundscore.api.urls')),
+
+    # granular views
     url(r'^api/locations/$', views.LocationList.as_view()),
     url(r'^api/locations/(?P<pk>[0-9]+)/$', views.LocationDetail.as_view()),
 
@@ -19,9 +22,18 @@ urlpatterns = patterns('',
     url(r'^api/measurements/$', views.MeasurementList.as_view()),
     url(r'^api/measurements/(?P<pk>[0-9]+)/$', views.MeasurementDetail.as_view()),
 
+    # todo view for measurements by sensor
+    # todo view for measurements by date range... slow down though, prob want to do that filtering upfront!
+
+    # aggregation views
     url(r'^api/hours/$', views.HourList.as_view()),
-    url(r'^api/hours/(?P<hour>[0-2]?[0-3])/$', views.HourDetailList.as_view()),
-    url(r'^api/hours/id/(?P<pk>[0-9]+)/$', views.HourDetail.as_view()),
+    url(r'^api/hours/(?P<hour>[0-2]?[0-9])/$', views.HourDetailList.as_view()),
+    # url(r'^api/hours/id/(?P<pk>[0-9]+)/$', views.HourDetail.as_view()),
+
+    # 1 = Sunday
+    url(r'^api/weekday/(?P<day>[1-7])/$', views.WeekDayDetailList.as_view()),
+    url(r'^api/monthday/(?P<day>[1-31])/$', views.MonthDayDetailList.as_view()),
+    url(r'^api/month/(?P<month>[1-3])/$', views.MonthDetailList.as_view()),
 
     # needed?
     url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
--- soundscore/views.py
+++ soundscore/views.py
@@ -2,16 +2,18 @@ from rest_framework import viewsets
 from rest_framework import generics
 from soundscore.models import Location, Sensor, Measurement, HourlySound
 from soundscore.serializers import LocationSerializer, SensorSerializer, MeasurementSerializer, HourSerializer
+from django.shortcuts import render
 
 __author__ = 'samrichards'
 
-from django.shortcuts import render
-
+# template views
 def home(request):
     locations = Location.objects.all()
     return render(request, 'home.html', {'locations': locations})
 
-# serializer list views
+# api views
+
+# list views
 class LocationList(generics.ListAPIView):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
@@ -35,22 +37,53 @@ class HourList(generics.ListAPIView):
     paginate_by_param = 'count'
     max_paginate_by = 1000000
 
+# detail list views
 class HourDetailList(generics.ListAPIView):
     serializer_class = HourSerializer
-    # lookup_field = 'hour'
     queryset = HourlySound.objects.all()
-    # serializer_class = HourSerializer
-    # paginate_by = 100
-    # paginate_by_param = 'count'
-    # max_paginate_by = 1000000
+    paginate_by = 100
+    paginate_by_param = 'count'
+    max_paginate_by = 1000000
 
     def get_queryset(self):
         hour_range = self.kwargs['hour']
         return HourlySound.objects.filter(hour__hour=hour_range)
 
+class WeekDayDetailList(generics.ListAPIView):
+    serializer_class = HourSerializer
+    queryset = HourlySound.objects.all()
+    paginate_by = 100
+    paginate_by_param = 'count'
+    max_paginate_by = 1000000
+
+    def get_queryset(self):
+        day_range = self.kwargs['day']
+        return HourlySound.objects.filter(hour__week_day=day_range)
+
+class MonthDayDetailList(generics.ListAPIView):
+    serializer_class = HourSerializer
+    queryset = HourlySound.objects.all()
+    paginate_by = 100
+    paginate_by_param = 'count'
+    max_paginate_by = 1000000
+
+    def get_queryset(self):
+        day_range = self.kwargs['day']
+        return HourlySound.objects.filter(hour__day=day_range)
+
+class MonthDetailList(generics.ListAPIView):
+    serializer_class = HourSerializer
+    queryset = HourlySound.objects.all()
+    paginate_by = 100
+    paginate_by_param = 'count'
+    max_paginate_by = 1000000
+
+    def get_queryset(self):
+        month = self.kwargs['month']
+        return HourlySound.objects.filter(hour__month=month)
 
 
-# serializer details views
+# detail views
 class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
