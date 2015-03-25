c447635cd68321d17fe113210f6047e8fce03a9e
Sam Richards <sbrichards@gmail.com>
2015-03-25 11:06:23 -0700

    Update local settings with RDS credentials

--- datacanvas/local_settings.py
+++ datacanvas/local_settings.py
@@ -3,8 +3,18 @@ import os
 __author__ = 'samrichards'
 
 
-
 if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -13,12 +23,5 @@ if 'RDS_DB_NAME' in os.environ:
             'PASSWORD': 'SoundScore4tw',
             'HOST': 'datacanvas-instance-1.c8ul9yqkcqls.us-west-1.rds.amazonaws.com',
             'PORT': '5432',
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'datacanvas',
         }
     }
